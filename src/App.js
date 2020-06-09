63d11c1016d9d32df4870829040d938d9b3755be
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-06-09 17:49:47 +0200

    Get Weather Function using async await

--- src/App.js
+++ src/App.js
@@ -3,13 +3,46 @@ import './App.css';
 import  Form from './Components/Form';
 import WeatherAPI from './Components/WeatherAPI'
 
+const API_KEYS ="49b665d9e0e16bb274cc2f37a9050557";
 
 class App extends Component{
 
+  state={
+    temperture :'',
+    city :'',
+    country :'',
+    humidity :'',
+    description :'',
+    error : ''
+  }
 
-getWeather=(e)=>{
-  e.preventDefault()
-  console.log("weather")
+
+getWeather= async (e)=>{
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEYS}`);
+    const data =await api.json();
+    
+    if(city && country){
+      this.setState({
+        temperture :data.main.temp,
+        city : data.name,
+        country : data.sys.country,
+        humidity :data.main.humidity,
+        description :data.weather[0].description,
+        error : ''
+      })
+    }else{
+      this.setState({
+          temperture :'',
+          city :'',
+          country :'',
+          humidity :'',
+          description :'',
+          error : 'Please Enter Data'}
+      )
+    }
 }
 
   render(){
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -8,8 +8,8 @@ const Form =(props) =>{
 
     return(
         <form onSubmit={props.getWeather}>
-            <input type="text"  name="city"placeholder="City..."/>
-            <input type="text" name ="country" placeholder="Country..."/>
+            <input type="text"   name="city" placeholder="City..."/>
+            <input type="text"  name ="country"  placeholder="Country..."/>
             <button>Get Weather</button>
          </form>
     )
